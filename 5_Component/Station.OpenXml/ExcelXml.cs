ba57b3b0b1e6d76acfe56fe826936e64d9e259e5
sun198986 <334828328@qq.com>
2020-08-06 18:31:34 +0800

    添加openxml

---
+++ 5_Component/Station.OpenXml/ExcelXml.cs
@@ -0,0 +1,39 @@
+﻿using System.Data;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace Station.OpenXml
+{
+    public static class ExcelXml
+    {
+        public static void ExcelTest(string excelOutput,DataTable dataTable)
+        {
+            using var spreadsheetDocument = SpreadsheetDocument.Create(excelOutput, SpreadsheetDocumentType.Workbook);
+            var workbookPart = spreadsheetDocument.AddWorkbookPart();
+            workbookPart.Workbook = new Workbook();
+            WorksheetPart worksheetPart = workbookPart.AddNewPart<WorksheetPart>();
+            worksheetPart.Worksheet = new Worksheet(new SheetData());
+            Sheets sheets = spreadsheetDocument.WorkbookPart.Workbook.AppendChild(new Sheets());
+            Sheet sheet = new Sheet() { Id = spreadsheetDocument.WorkbookPart.GetIdOfPart(worksheetPart), SheetId = 1, Name = "Sheet1" };
+            sheets.Append(sheet);
+            var sheetData = worksheetPart.Worksheet.GetFirstChild<SheetData>();
+            #region 此处换成datatable
+            for (int i = 0; i < 10; i++)
+            {
+                Row row = new Row();
+                for (int j = 0; j < 10; j++)
+                {
+                    Cell dataCell = new Cell();
+                    dataCell.CellValue = new CellValue($"{i + 1}行{j + 1}列");
+                    dataCell.DataType = new EnumValue<CellValues>(CellValues.String);
+                    row.AppendChild(dataCell);
+                }
+                sheetData.Append(row);
+            }
+            #endregion
+            workbookPart.Workbook.Save();
+            spreadsheetDocument.Close();
+        }
+    }
+}
